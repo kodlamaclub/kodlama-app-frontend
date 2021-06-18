5cc91fafca36815d1ac74ea4aa92b4088b5e789f
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-18 23:26:55 +0300

    Add header component

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {LayoutComponent} from './layout/layout.component';
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import {HeaderComponent} from "@design/header/header.component";
 
 @NgModule({
   declarations: [
     LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     RouterModule,
