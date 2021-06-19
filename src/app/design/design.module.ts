ce6bab6a91894085f9ad3def8f8aab0a7c5dbe9f
fishuke <fishukee@gmail.com>
2021-06-19 20:06:52 +0300

    Fix misplaced width

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -7,20 +7,24 @@ import {MatIconModule} from "@angular/material/icon";
 import {FlexModule} from "@angular/flex-layout";
 import { LogoComponent } from './logo/logo.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import { UserComponent } from './user/user.component';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     LayoutComponent,
     SidenavComponent,
-    LogoComponent
+    LogoComponent,
+    UserComponent
   ],
-  imports: [
-    RouterModule,
-    CommonModule,
-    MatIconModule,
-    FlexModule,
-    MatSidenavModule,
-  ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        MatIconModule,
+        FlexModule,
+        MatSidenavModule,
+        MatButtonModule,
+    ],
 })
 export class DesignModule {
 }
