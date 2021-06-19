e4394ec357d0b3080bc6647947753e528415b305
fishuke <fishukee@gmail.com>
2021-06-19 19:32:37 +0300

    Add logo component

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -3,15 +3,23 @@ import {LayoutComponent} from './layout/layout.component';
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {SidenavComponent} from "./sidenav/sidenav.component";
+import {MatIconModule} from "@angular/material/icon";
+import {FlexModule} from "@angular/flex-layout";
+import { LogoComponent } from './logo/logo.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    SidenavComponent
+    SidenavComponent,
+    LogoComponent
   ],
   imports: [
     RouterModule,
     CommonModule,
+    MatIconModule,
+    FlexModule,
+    MatSidenavModule,
   ],
 })
 export class DesignModule {
