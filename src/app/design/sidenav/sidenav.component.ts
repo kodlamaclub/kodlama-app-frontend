c19224b0210aecaae9f9865b47c76127d37503c3
fishuke <fishukee@gmail.com>
2021-06-19 19:33:48 +0300

    Add cool things ✨

--- src/app/design/sidenav/sidenav.component.ts
+++ src/app/design/sidenav/sidenav.component.ts
@@ -7,42 +7,47 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  currentItem: any;
+  menuItems = [
+    {
+      icon: 'dashboard',
+      title: 'Anasayfa',
+      path: 'home'
+    },
+    {
+      icon: 'book',
+      title: 'Kurslarım',
+      path: 'courses'
+    },
+    {
+      icon: 'heart',
+      title: 'Favorilerim',
+      path: 'favorites'
+    }, {
+      icon: 'notepad',
+      title: 'Ödevlerim',
+      path: 'tasks'
+    },
+    {
+      icon: 'award',
+      title: 'Başarılarım',
+      path: 'achievements'
+    },
+    {
+      icon: 'certificate',
+      title: 'Sertifikalarım',
+      path: 'certificates'
+    },
+    {
+      icon: 'settings',
+      title: 'Ayarlar',
+      path: 'settings'
+    },
+  ]
 
   constructor() {
   }
 
   ngOnInit(): void {
 
-  }
-
-  homePage = {name: `<i class="material-icons">home</i>Anasayfa`}
-  courses = {name: `<i class="material-icons">class</i> Kurslarım`}
-  favorites = {name: `<i class="material-icons">favorite_border</i> Favorilerim`}
-  homework = {name: `<i class="material-icons">auto_stories</i> Ödevlerim`}
-  achievements = {name: `<i class="material-icons">military_tech</i> Başarılarım`}
-  certificates = {name: `<i class="material-icons">card_membership</i> Sertifikalarım`}
-  settings = {name: `<i class="material-icons">settings</i> Ayarlar`}
-
-  items = [this.homePage, this.courses, this.favorites, this.homework, this.achievements, this.certificates, this.settings]
-
-  setCurrentItem(item: any) {
-    this.currentItem = item;
-  }
-
-  getCurrentItemClass(item: any) {
-    if (item == this.currentItem) {
-      return "mb-6 click"
-    } else {
-      return "mb-6"
-    }
-  }
-
-  getItemClass() {
-    if (!this.currentItem) {
-      return "mb-6 click"
-    } else {
-      return "mb-6"
-    }
   }
 }
