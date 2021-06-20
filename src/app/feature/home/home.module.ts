66e3480679b4d97d796d473d52d067e61ddeff9f
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-20 16:56:36 +0300

    Add browse component

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {DesignModule} from "@design/design.module";
+import {BrowseComponent} from "@feature/home/browse/browse.component";
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BrowseComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    DesignModule
   ]
 })
 export class HomeModule { }
