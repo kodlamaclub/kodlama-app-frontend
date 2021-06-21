13c9d9d1502f33b98e97c3dca82e9c00c1198643
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-21 21:52:02 +0300

    Add modules

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -1,21 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
 import {DesignModule} from "@design/design.module";
 import {BrowseComponent} from "@feature/home/browse/browse.component";
+import {MatIconModule} from "@angular/material/icon";
+import {FlexModule} from "@angular/flex-layout";
+import {MatInputModule} from "@angular/material/input";
+import { CourseListItemComponent } from './course-list-item/course-list-item.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    BrowseComponent
+    BrowseComponent,
+    CourseListItemComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    DesignModule
+    DesignModule,
+    MatIconModule,
+    FlexModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
