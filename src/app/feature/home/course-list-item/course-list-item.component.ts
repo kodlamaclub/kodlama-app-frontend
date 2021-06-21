d5d0f252b70f34fce90fd24f9fd6712adaff401c
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-21 21:43:48 +0300

    Add input

---
+++ src/app/feature/home/course-list-item/course-list-item.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-course-list-item',
+  templateUrl: './course-list-item.component.html',
+  styleUrls: ['./course-list-item.component.scss']
+})
+export class CourseListItemComponent implements OnInit {
+  @Input() course
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
