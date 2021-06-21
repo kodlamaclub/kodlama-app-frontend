89cd6eb670e9a927edcaff5a683f9b75f52b4a4c
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-21 21:49:08 +0300

    Add courses

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -19,4 +19,27 @@ export class HomeComponent implements OnInit {
       content:"Nestjs, verimli, güvenilir ve ölçeklenebilir sunucu tarafı uygulamaları oluşturmak için bir Node.js frameworku."
     }
   ]
+
+  courses=[
+    {
+      image:"mobile-app",
+      title:"Mobil Uygulama Geliştirme",
+      kind:"Ionic"
+    },
+    {
+      image:"google",
+      title:"Google Chrome Eklenti Geliştirme",
+      kind:"Angular"
+    },
+    {
+      image:"firebase",
+      title:"Firebase ile Web Geliştirme ",
+      kind:"Firebase"
+    },
+    {
+      image:"amazon-web-services",
+      title:"Serverless Geliştirme",
+      kind:"Amazon Web Services"
+    }
+  ]
 }
